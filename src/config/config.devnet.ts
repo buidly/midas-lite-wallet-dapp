ddd31f0014f0443ba21381806b08b02fd43a364e
IosifGabriel <gabriel.iosif06@gmail.com>
2024-06-25 13:34:25 +0300

    update

--- src/config/config.devnet.ts
+++ src/config/config.devnet.ts
@@ -4,7 +4,7 @@ export * from './sharedConfig';
 
 export const contractAddress =
   'erd1qqqqqqqqqqqqqpgqm6ad6xrsjvxlcdcffqe8w58trpec09ug9l5qde96pq';
-export const API_URL = 'https://devnet-template-api.multiversx.com';
+export const API_URL = 'https://api.internal.midaschain.ai';
 export const sampleAuthenticatedDomains = [API_URL];
 export const environment = EnvironmentsEnum.devnet;
 export const explorerAddress = 'https://explorer.internal.midaschain.ai';
--- src/config/config.mainnet.ts
+++ src/config/config.mainnet.ts
@@ -4,7 +4,7 @@ export * from './sharedConfig';
 
 export const contractAddress =
   'erd1qqqqqqqqqqqqqpgqtmcuh307t6kky677ernjj9ulk64zq74w9l5qxyhdn7';
-export const API_URL = 'https://template-api.multiversx.com';
+export const API_URL = 'https://api.internal.midaschain.ai';
 export const sampleAuthenticatedDomains = [API_URL];
 export const environment = EnvironmentsEnum.mainnet;
 export const explorerAddress = 'https://explorer.internal.midaschain.ai';
