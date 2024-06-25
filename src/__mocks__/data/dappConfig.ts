9930ac1a449e8bc1861897e4f38dd138442e0366
IosifGabriel <gabriel.iosif06@gmail.com>
2024-06-25 13:24:50 +0300

    update some configs for midas

--- src/__mocks__/data/dappConfig.ts
+++ src/__mocks__/data/dappConfig.ts
@@ -1,7 +1,7 @@
 export const dappConfig = {
-  id: 'devnet',
-  name: 'Devnet',
-  egldLabel: 'xEGLD',
+  id: 'midas-testnet',
+  name: 'midas-testnet',
+  egldLabel: 'OURO',
   decimals: '4',
   egldDenomination: '18',
   gasPerDataByte: '1500',
@@ -9,8 +9,8 @@ export const dappConfig = {
   walletConnectDeepLink:
     'https://maiar.page.link/?apn=com.multiversx.maiar.wallet&isi=1519405832&ibi=com.multiversx.maiar.wallet&link=https://maiar.com/',
   walletConnectBridgeAddresses: ['https://bridge.walletconnect.org'],
-  walletAddress: 'https://devnet-wallet.multiversx.com',
-  apiAddress: 'https://devnet-api.multiversx.com',
-  explorerAddress: 'http://devnet-explorer.multiversx.com',
-  chainId: 'D'
+  walletAddress: 'https://testnet-wallet.midaschain.ai',
+  apiAddress: 'https://api.internal.midaschain.ai',
+  explorerAddress: 'https://explorer.internal.midaschain.ai',
+  chainId: 'local-testnet'
 };
